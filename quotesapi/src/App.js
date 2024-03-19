942445bec62a62dd1b2f1f27e0aaf29cd8afc487
sheetal <sheetal.kulkarni91@gmail.com>
2024-03-19 15:06:56 -0700

    version1

--- quotesapi/src/App.js
+++ quotesapi/src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
+import React from "react"
 
 function App() {
+  // const apiKey = 'PPacgVuugv5uNHsIBRcQi3RE7Vd4z7p6v4UZBuOv';
+  const [todos, setTodos] = React.useState([])
+  React.useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/todos")
+      .then(response => response.json())
+      .then(data => {
+        setTodos(data);
+
+      })
+      .catch(error => console.error('Error fetching todos:', error));
+  }, []);
+  let trueTodos = todos.filter(todo => todo.completed === true)
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ textAlign: 'center' }}>
+      <ul style={{ listStyle: "none" }}>{trueTodos.map((todo, id) => <li key={todo.id} style={{ textAlign: 'left', width: '40vw', padding: '20px' }}>
+        <span style={{ marginRight: '20px' }}>ID-{todo.id}</span>
+        <span style={{ color: 'blue', fontStyle: 'italic' }} >{todo.title}</span>
+      </li>
+      )}</ul>
     </div>
   );
 }
